50f9f5c98a514eef276a35d98cc9547431853a60
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2018-03-18 16:31:19 -0400

    Moving the .git file

---
+++ src/main/java/orders/OrderQueue.java
@@ -0,0 +1,29 @@
+package orders;
+import java.util.ArrayList;
+import java.util.List;
+
+// This class is a singleton that contains all of the orders that are not done.
+public class OrderQueue {
+    private List<Order> orders;
+    private static OrderQueue instance;
+
+    // Private to prevent external classes from creating an instance.
+    private OrderQueue() {
+        orders = new ArrayList<>();
+
+    }
+
+    // Method to access the instance of the singleton.
+    public static OrderQueue getInstance() {
+        if(instance == null) {
+            instance = new OrderQueue();
+        }
+
+        return instance;
+    }
+
+
+
+
+
+}
